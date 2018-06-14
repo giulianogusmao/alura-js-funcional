d81e49567f83018a939a4bf30ee203febca6634f
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-14 11:12:41 -0300

    organizando a soma das notas

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,17 +1,18 @@
 import './utils/array-helpers.js';
 import { handleStatus, log } from './utils/promise-helpers.js';
 
+const sumItems = notas =>
+    notas
+        .$flatMap(nota => nota.itens)
+        .filter(item => item.codigo == '2143')
+        .reduce((total, item) => total + item.valor, 0)
 
 document
     .querySelector('#myButton')
     .onclick = () => {
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(notas => notas.$flatMap(nota => nota.itens))
-            .then(log)
-            .then(itens => itens.filter(item => item.codigo == '2143'))
-            .then(log)
-            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+            .then(sumItems)
             .then(log)
             .catch(console.error);
     };
