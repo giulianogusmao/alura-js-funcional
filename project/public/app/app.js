1a05e13520aa8e7a8368014ea01f4b73c3cc684f
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-14 11:09:51 -0300

    somando todas as notas de um determinado codigo

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,4 +1,5 @@
-import { handleStatus } from './utils/promise-helpers.js';
+import './utils/array-helpers.js';
+import { handleStatus, log } from './utils/promise-helpers.js';
 
 
 document
@@ -6,6 +7,11 @@ document
     .onclick = () => {
         fetch('http://localhost:3000/notas')
             .then(handleStatus)
-            .then(console.log)
+            .then(notas => notas.$flatMap(nota => nota.itens))
+            .then(log)
+            .then(itens => itens.filter(item => item.codigo == '2143'))
+            .then(log)
+            .then(itens => itens.reduce((total, item) => total + item.valor, 0))
+            .then(log)
             .catch(console.error);
     };
