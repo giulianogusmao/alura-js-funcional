fba4d3b2414c5d5756ef12bc1b6a329b0e9b3868
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-14 11:46:07 -0300

    Passando responsabilidade de somar notas para o servico de notas

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,19 +1,11 @@
 import './utils/array-helpers.js';
 import { log } from './utils/promise-helpers.js';
-
 import { notasService as service } from './nota/service.js';
-
-const sumItems = code => notas =>
-    notas
-        .$flatMap(nota => nota.itens)
-        .filter(item => item.codigo == code)
-        .reduce((total, item) => total + item.valor, 0)
 
 document
     .querySelector('#myButton')
     .onclick = () => {
         service
-            .listAll()
-            .then(sumItems('2143'))
+            .sumItems('2143')
             .then(log);
     };
--- project/public/app/nota/service.js
+++ project/public/app/nota/service.js
@@ -2,10 +2,15 @@ import { handleStatus } from '../utils/promise-helpers.js';
 
 const API = 'http://localhost:3000/notas';
 
-
+const sumItems = code => notas =>
+    notas
+        .$flatMap(nota => nota.itens)
+        .filter(item => item.codigo == code)
+        .reduce((total, item) => total + item.valor, 0)
 
 export const notasService = {
-    listAll: () => {
+    // carrega notas
+    listAll() {
         return fetch(API)
             .then(handleStatus)
             .catch(err => {
@@ -13,7 +18,11 @@ export const notasService = {
                 return Promise.reject('Não foi possível obter as notas fiscais');
             });
     },
-    sumItems: code => {
-        return this.listAll();
+
+    // soma todas as notas de um determinado codigo
+    sumItems(code) {
+        return this
+            .listAll()
+            .then(sumItems(code));
     },
 };
