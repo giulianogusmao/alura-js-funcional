806fbe63c6ceae2fa9df65b3459f2f65ed28c6cf
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-14 16:08:48 -0300

    Refatorando functions em sequencia para utilizar o composer

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,17 +1,19 @@
 import './utils/array-helpers.js';
 import { log } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
-import { takeUntil, debounceTime } from './utils/operators.js';
+import { takeUntil, debounceTime, partialize, composer } from './utils/operators.js';
 
-const getTotalNotas = () => {
+const operation = composer(
+    partialize(debounceTime, 500),
+    partialize(takeUntil, 3)
+);
+
+const action = operation(() => {
     service
         .sumItems('2143')
         .then(log)
         .catch(console.error);
-};
-
-const operation = takeUntil(3, getTotalNotas);
-const action = debounceTime(500, operation);
+});
 
 document
     .querySelector('#myButton')
