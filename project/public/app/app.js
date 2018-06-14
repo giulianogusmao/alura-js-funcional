ef0311af2964558cd4d3ffe1900b8986c6836979
Giuliano Gusmão <gusmaoggg@gmail.com>
2018-06-14 17:59:09 -0300

    Documentando e criando retry p/ realizar novas tentativas de requisicao

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,5 +1,5 @@
 import './utils/array-helpers.js';
-import { timeoutPromise, delayPromise as delay } from './utils/promise-helpers.js';
+import { timeoutPromise, delayPromise as delay, retry } from './utils/promise-helpers.js';
 import { notasService as service } from './nota/service.js';
 import { log, takeUntil, debounceTime, partialize, composer } from './utils/operators.js';
 
@@ -8,8 +8,10 @@ const operation = composer(
     partialize(takeUntil, 3)
 );
 
+const calcTotal = () => timeoutPromise(200, service.sumItems('2143'));
+
 const action = operation(() => {
-    timeoutPromise(200, service.sumItems('2143'))
+    retry(3, 3000, calcTotal)
         .then(delay(2000))
         .then(log)
         .catch(console.error);
--- project/public/app/utils/promise-helpers.js
+++ project/public/app/utils/promise-helpers.js
@@ -1,6 +1,19 @@
+/**
+ * Realiza o parse para json() da primeira resposta da promise caso
+ * o response seja ok ou rejeita caso tenha algum problema.
+ * 
+ * @param {response} res 
+ */ 
 export const handleStatus = res =>
     res.ok ? res.json() : Promise.reject(res.statusText);
 
+
+/**
+ * Determina um tempo limite para aguarada a resposta de uma promise
+ * 
+ * @param {number} milliseconds - Tempo limite da resposta
+ * @param {promise} promise - Requisição
+ */
 export const timeoutPromise = (milliseconds, promise) => {
     const timeout = new Promise((resolve, reject) => {
         setTimeout(() => {
@@ -14,7 +27,33 @@ export const timeoutPromise = (milliseconds, promise) => {
     ]);
 };
 
+
+/**
+ * Determina um tempo de espera em milisegundos que o parâmetro recebido 
+ * será passado adiante.
+ * 
+ * @param {number} milliseconds - Tempo de espera da resposta
+ */
 export const delayPromise = (milliseconds) => value =>
     new Promise(resolve => setTimeout(() => {
         resolve(value);
     }, milliseconds));
+
+
+/**
+ * Sempre que uma promise retornada da fn for executada e falhar a Retry
+ * irá realizar um dterminado número tentativas(retries) dentro de um 
+ * intervalo de tempo(milliseconds).
+ * 
+ * @param {number} retries - Numero de tentativas
+ * @param {number} milliseconds - Tempo entre cada tentativa
+ * @param {function} fn - função que retorna uma promise com a requisição
+ */
+export const retry = (retries, milliseconds, fn) =>
+    fn().catch(err => {
+        console.log(retries);
+        return delayPromise(milliseconds)().then(() =>
+            retries > 1
+                ? retry(retries - 1, milliseconds, fn)
+                : Promise.reject(err));
+    });
